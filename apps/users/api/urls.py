b3095c3188c6162894f786a6bc982fd3fa016f65
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2022-08-24 09:23:39 -0300

    create, update, delete usuario

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.users.api.api import user_api_view
+from apps.users.api.api import user_api_view, user_detail_view
 
 urlpatterns = [
-    path('', user_api_view, name="usuarios_api")
+    path('', user_api_view, name="usuarios_api"),
+    path('<int:pk>', user_detail_view, name="usuario_detalle")
 ]
