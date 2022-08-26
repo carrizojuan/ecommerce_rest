a97467e30275c70b5865fe964e3821de2fe9d01b
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2022-08-26 11:10:59 -0300

    createApiView

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from apps.products.api.views.general import MeasureUnitListApiView, CategoryProductListApiView, IndicatorListApiView
-from apps.products.api.views.product import ProductListApiView
+from apps.products.api.views.product import ProductListApiView, ProductCreateApiView
 
 urlpatterns = [
     path('measure-units/', MeasureUnitListApiView.as_view(), name="measure_units"),
     path('category-products/', CategoryProductListApiView.as_view(), name="category_products"),
     path('indicators/', IndicatorListApiView.as_view(), name="indicators"),
+    path('product/create', ProductCreateApiView.as_view(), name="product_create"),
     path('product/', ProductListApiView.as_view(), name="products"),
+    
 ]
--- apps/products/api/views/product.py
+++ apps/products/api/views/product.py
@@ -1,5 +1,22 @@
+from itertools import product
+from os import stat
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status 
+
 from apps.base.api import GenericListApiView
 from apps.products.api.serializers.product import ProductSerializer
 
 class ProductListApiView(GenericListApiView):
     serializer_class = ProductSerializer
+
+
+class ProductCreateApiView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def post(self, request):
+        product_serializer = self.serializer_class(data=request.data)
+        if product_serializer.is_valid():
+            product_serializer.save()
+            return Response({'message': 'Producto creado correctamente'}, status=status.HTTP_201_CREATED)
+        return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
